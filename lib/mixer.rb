5d6b309fbaa4d7e72552164ac6571f6da3cc6b44
Brandon Randall <br.mhcexchange@gmail.com>
2017-05-17 21:03:44 -0600

    complete ability to make multiple picks with prevoius picks taken out of cage

--- lib/mixer.rb
+++ lib/mixer.rb
@@ -2,30 +2,37 @@ require './lib/team_ball'
 
 class Mixer
 
-  attr_reader :cage, :count
+  attr_reader :cage, :count, :current_pick
 
   def initialize(cage = [])
     @cage = cage
     @count = count
+    @picks = []
+  end
+
+  def current_pick
+    @picks.last
   end
 
   def balls_in_cage
     # @cage.reduce(:+)
     @cage.count
+    # binding.pry
   end
 
   def choose
     chosen = @cage.sample
     # puts chosen.team
-    chosen.team
-  end
-
-  def scale
-    957.times {puts choose}
     # binding.pry
+    chosen_team = chosen.team
+    #need to take the selected object and insert into the chosen array
+    @picks.push(chosen_team)
+    #then need to delete all objects in the cage that have the same name
+    @cage = @cage.select { |team| team.team != chosen_team }
+
   end
 
-end
+  end
 
 # mixer = Mixer.new
 # puts mixer.choose
--- test/draft_lottery_test.rb
+++ test/draft_lottery_test.rb
@@ -6,84 +6,77 @@ require './lib/mixer'
 
 class DraftLotteryTest < Minitest::Test
 
+  def setup
+
+    @mixer = Mixer.new
+
+    celtics = TeamBall.new("Boston Celtics")
+    @celtics_balls = 250.times {@mixer.cage.push(celtics)}
+    suns = TeamBall.new("Phoenix Suns")
+    @suns_balls = 199.times {@mixer.cage.push(suns)}
+    lakers = TeamBall.new("Los Angeles Lakers")
+    @lakers_balls = 156.times {@mixer.cage.push(lakers)}
+    sixers = TeamBall.new("Philadelphia 76ers")
+    @sixers_balls = 119.times {@mixer.cage.push(sixers)}
+    magic = TeamBall.new("Orlando Magic")
+    @magic_balls = 88.times {@mixer.cage.push(magic)}
+    knicks = TeamBall.new("New York Knicks")
+    @knicks_balls = 53.times {@mixer.cage.push(knicks)}
+    kings = TeamBall.new("Sacramento Kings")
+    @kings_balls = 28.times {@mixer.cage.push(kings)}
+    mavs = TeamBall.new("Dallas Mavericks")
+    @mavs_balls = 17.times {@mixer.cage.push(mavs)}
+    kings_from_pelicans = TeamBall.new("New Orleans Pelicans")
+    @kings_from_pelicans_balls = 11.times {@mixer.cage.push(kings_from_pelicans)}
+    hornets = TeamBall.new("Charlotte Hornets")
+    @hornets_balls = 8.times {@mixer.cage.push(hornets)}
+    pistons = TeamBall.new("Detroit Pistons")
+    @pistons_balls = 7.times {@mixer.cage.push(pistons)}
+    nuggets = TeamBall.new("Denver Nuggets")
+    @nuggets_balls = 6.times {@mixer.cage.push(nuggets)}
+    heat = TeamBall.new("Miami Heat")
+    @heat_balls = 5.times {@mixer.cage.push(heat)}
+
+  end
+
   def test_teams_have_correct_amounts_of_balls
 
-    mixer = Mixer.new
-
-    celtics = TeamBall.new("Boston Celtics")
-    celtics_balls = 250.times {mixer.cage.push(celtics)}
-    suns = TeamBall.new("Phoenix Suns")
-    suns_balls = 199.times {mixer.cage.push(suns)}
-    lakers = TeamBall.new("Los Angeles Lakers")
-    lakers_balls = 156.times {mixer.cage.push(lakers)}
-    sixers = TeamBall.new("Philadelphia 76ers")
-    sixers_balls = 119.times {mixer.cage.push(sixers)}
-    magic = TeamBall.new("Orlando Magic")
-    magic_balls = 88.times {mixer.cage.push(magic)}
-    knicks = TeamBall.new("New York Knicks")
-    knicks_balls = 53.times {mixer.cage.push(knicks)}
-    kings = TeamBall.new("Sacramento Kings")
-    kings_balls = 28.times {mixer.cage.push(kings)}
-    mavs = TeamBall.new("Dallas Mavericks")
-    mavs_balls = 17.times {mixer.cage.push(mavs)}
-    kings_from_pelicans = TeamBall.new("New Orleans Pelicans")
-    kings_from_pelicans_balls = 11.times {mixer.cage.push(kings_from_pelicans)}
-    hornets = TeamBall.new("Charlotte Hornets")
-    hornets_balls = 8.times {mixer.cage.push(hornets)}
-    pistons = TeamBall.new("Detroit Pistons")
-    pistons_balls = 7.times {mixer.cage.push(pistons)}
-    nuggets = TeamBall.new("Denver Nuggets")
-    nuggets_balls = 6.times {mixer.cage.push(nuggets)}
-    heat = TeamBall.new("Miami Heat")
-    heat_balls = 5.times {mixer.cage.push(heat)}
-
-    assert_equal 250, celtics_balls
-    assert_equal 156, lakers_balls
-    assert_equal 6, nuggets_balls
+    assert_equal 250, @celtics_balls
+    assert_equal 156, @lakers_balls
+    assert_equal 6, @nuggets_balls
 # count the number of balls in the cage
-
-    assert_equal 947, mixer.balls_in_cage
+    assert_equal 947, @mixer.balls_in_cage
   end
 
-  def test_cage_has_random_picks
+  def test_cage_can_make_one_pick
+    teams_not_picked    = @mixer.choose
+    current_picked_team = @mixer.current_pick
 
-    mixer = Mixer.new
-
-    celtics = TeamBall.new("Boston Celtics")
-    celtics_balls = 250.times {mixer.cage.push(celtics)}
-    suns = TeamBall.new("Phoenix Suns")
-    suns_balls = 199.times {mixer.cage.push(suns)}
-    lakers = TeamBall.new("Los Angeles Lakers")
-    lakers_balls = 156.times {mixer.cage.push(lakers)}
-    sixers = TeamBall.new("Philadelphia 76ers")
-    sixers_balls = 119.times {mixer.cage.push(sixers)}
-    magic = TeamBall.new("Orlando Magic")
-    magic_balls = 88.times {mixer.cage.push(magic)}
-    knicks = TeamBall.new("New York Knicks")
-    knicks_balls = 53.times {mixer.cage.push(knicks)}
-    kings = TeamBall.new("Sacramento Kings")
-    kings_balls = 28.times {mixer.cage.push(kings)}
-    mavs = TeamBall.new("Dallas Mavericks")
-    mavs_balls = 17.times {mixer.cage.push(mavs)}
-    kings_from_pelicans = TeamBall.new("New Orleans Pelicans")
-    kings_from_pelicans_balls = 11.times {mixer.cage.push(kings_from_pelicans)}
-    hornets = TeamBall.new("Charlotte Hornets")
-    hornets_balls = 8.times {mixer.cage.push(hornets)}
-    pistons = TeamBall.new("Detroit Pistons")
-    pistons_balls = 7.times {mixer.cage.push(pistons)}
-    nuggets = TeamBall.new("Denver Nuggets")
-    nuggets_balls = 6.times {mixer.cage.push(nuggets)}
-    heat = TeamBall.new("Miami Heat")
-    heat_balls = 5.times {mixer.cage.push(heat)}
-
-    lucky_first = TeamBall
-    lucky_second = TeamBall
-    lucky_third = TeamBall
-
-    assert_equal String, mixer.choose.class
-    # how is this an integer?
-    assert_equal Integer, mixer.scale.class
-
+    refute teams_not_picked.include?(current_picked_team)
+    assert_equal Integer, @mixer.balls_in_cage.class
+    assert (@mixer.balls_in_cage < 947), "Mixer count is not less than 947."
   end
 
+  def test_cage_can_make_more_than_one_pick
+    teams_not_picked    = @mixer.choose
+    first_pick          = @mixer.current_pick
+    teams_not_picked    = @mixer.choose
+    current_picked_team = @mixer.current_pick
+# binding.pry
+    refute first_pick == current_picked_team
+  end
+
+  def test_cage_can_make_3_picks
+    teams_not_picked    = @mixer.choose
+    first_pick          = @mixer.current_pick
+    teams_not_picked    = @mixer.choose
+    second_pick         = @mixer.current_pick
+    teams_not_picked    = @mixer.choose
+    third_pick          = @mixer.current_pick
+
+    refute first_pick == third_pick
+    binding.pry
+    refute second_pick == third_pick
+
+  end
 end
